a3ce308a4172feb750a31419da48949e79fe5160
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-05-21 10:04:53 +0200

    Update add.php

--- src/add.php
+++ src/add.php
@@ -53,8 +53,8 @@ if(isset($_POST['Submit'])) {
 		// if all the fields are filled (not empty)
 
 		// insert data to database
-		$stmt = mysqli_prepare($mysqli, "INSERT INTO users(name,age, apellido1, apellido2, email) VALUES(?,?,?)");
-		mysqli_stmt_bind_param($stmt, $name, $age, $apellido1, $apellido2, $email);
+		$stmt = mysqli_prepare($mysqli, "INSERT INTO users(name, apellido1, apellido2, age, email) VALUES(?,?,?)");
+		mysqli_stmt_bind_param($stmt, "sssis" $name, $apellido1, $apellido2, $age, $email);
 		mysqli_stmt_execute($stmt);
 		mysqli_stmt_free_result($stmt);
 		mysqli_stmt_close($stmt);
